136aa6d3c782e26272a2a8a7fffe83807b3a02e6
Johann.Casallas <Johann.Casallas@infotrack.com.co>
2023-10-18 10:53:40 -0500

    Agregar archivos de proyecto.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="movilNavBackend.WebApiApplication" Language="C#" %>
---
+++ Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace movilNavBackend.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string DocumnetosIdentidad { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Ciudad { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+
+    }
+}
